a3738399702a2609882392bdb38d0b5a1e2e14fc
troyfoster <troy@troyfoster.net>
2024-04-15 06:07:22 -0700

    Update first_streamlit_app.py
    Added test code to make sure connection and secrets are working.

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -3,4 +3,11 @@ import snowflake.connector
 
 streamlit.title('My Parents\' New Healthy Diner')
 
+my_conx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(), current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hellow from Snowflake:")
+streamlit.text(my_data_row)
+
 
