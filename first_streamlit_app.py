fb8e9959f452b26b42e2377772651c8eb5720d0b
troyfoster <troy@troyfoster.net>
2024-04-15 06:08:03 -0700

    Update first_streamlit_app.py
    Fixed connection name.

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 
 streamlit.title('My Parents\' New Healthy Diner')
 
-my_conx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select current_user(), current_account(), current_region()")
 my_data_row = my_cur.fetchone()
